0bbf27294428c8aad807dd537e58263b12c0b1ce
vigny-Domou <domouvigny@gmail.com>
2023-06-27 16:17:47 +0100

    quelque motification sur les noms de variables

--- lib/src/Model/Transaction.dart
+++ lib/src/Model/Transaction.dart
@@ -26,28 +26,28 @@ class Transaction {
   late List<Product>? products;
 
   Transaction(Map<String, dynamic> data) {
-    this.pk = data['pk'];
-    this.success = data['status'];
-    this.type = data['type'];
-    this.amount = data['amount'];
-    this.fees = data['fees'];
-    this.b_party = data['b_party'];
-    this.message = data['message'];
-    this.service = data['service'];
-    this.reference = data['reference'];
-    this.ts = data['ts'];
-    this.country = data['country'];
-    this.currency = data['currency'];
-    this.fin_trx_id = data['fin_trx_id'];
-    this.trxamount = data['trxamount'];
+    pk = data['pk'];
+    success = data['status'];
+    type = data['type'];
+    amount = data['amount'];
+    fees = data['fees'];
+    b_party = data['b_party'];
+    message = data['message'];
+    service = data['service'];
+    reference = data['reference'];
+    ts = data['ts'];
+    country = data['country'];
+    currency = data['currency'];
+    fin_trx_id = data['fin_trx_id'];
+    trxamount = data['trxamount'];
     if (data['location'] != null) {
-      this.location = Location(data['location']);
+      location = Location(data['location']);
     }
     if (data['customer'] != null) {
-      this.customer = Customer(data['customer']);
+      customer = Customer(data['customer']);
     }
     if (data['products'] != null) {
-      this.products =
+      products =
           data['products'].map((d) => Product(d)).toList().cast<Product>();
     }
   }
